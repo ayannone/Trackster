ac201af557a84f70e3858bc5d74a447996b4137f
atrapp <andrea.trapp@gmx.net>
2017-11-26 09:44:22 -0500

    Initial commit - Add HTML and CSS to Header section

---
+++ resources/js/trackster.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  var Trackster = {};
+
+  $('#search-button').click(function() {
+    var searchValue = $('#search-input').val();
+    Trackster.searchTracksByTitle(searchValue);
+  });
+
+  /*
+    Given an array of track data, create the HTML for a Bootstrap row for each.
+    Append each "row" to the container in the body to display all tracks.
+  */
+  Trackster.renderTracks = function(tracks) {
+
+  };
+
+  /*
+    Given a search term as a string, query the LastFM API.
+    Render the tracks given in the API query response.
+  */
+  Trackster.searchTracksByTitle = function(title) {
+
+  };
+
+})
