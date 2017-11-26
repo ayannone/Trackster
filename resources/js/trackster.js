4894ff27d6900725dcb05a981c69287a53986ab3
atrapp <andrea.trapp@gmx.net>
2017-11-26 11:49:06 -0500

    Display tracks on web page

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
   var Trackster = {};
 
   $('#search-button').click(function() {
-    var searchValue = $('#search-input').val();
-    Trackster.searchTracksByTitle(searchValue);
+    var $searchValue = $('#search-input').val();
+    Trackster.searchTracksByTitle($searchValue);
   });
 
   /*
@@ -14,7 +14,40 @@ $(document).ready(function() {
     Append each "row" to the container in the body to display all tracks.
   */
   Trackster.renderTracks = function(tracks) {
+    // console.log(tracks);
+    var amountTracks = tracks.results.trackmatches.track.length;
 
+    for (i = 0; i < amountTracks; i++) {
+      var currentTrack = tracks.results.trackmatches.track[i];
+
+      var url = currentTrack['url'];
+      var song = currentTrack['name'];
+      var artist = currentTrack['artist'];
+      var albumart = currentTrack['image'][0]['#text'];
+      var listeners = currentTrack['listeners'];
+
+      var $track = $(
+        "<div class='row'>" +
+          "<div class='col-xs-1 col-xs-offset-1 play-button'>" +
+          "<a href='" + url + "' target='_blank' ><i class='fa fa-play-circle-o fa-2x'></i></a>" +
+        "</div>" +
+        "<div class='col-xs-4'>" +
+          "<span>" + song + "</span>" +
+        "</div>" +
+        "<div class='col-xs-2'>" +
+          "<span>" + artist + "</span>" +
+        "</div>" +
+        "<div class='col-xs-2'>" +
+          "<span>" + albumart + "</span>" +
+        "</div>" +
+        "<div class='col-xs-2'>" +
+          "<span>" + listeners + "</span>" +
+        "</div>" +
+      "</div>");
+
+      // console.log($track);
+      $('#tracklist').append($track);
+    }
   };
 
   /*
@@ -26,7 +59,7 @@ $(document).ready(function() {
       url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
       datatype: 'jsonp',
       success: function(data) {
-        console.log(data);
+        Trackster.renderTracks(data);
       }
     })
   };
